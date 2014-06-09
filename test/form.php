3948fa68114ac4328678b9d186876e6368fe43b0
igarbt <igarbt@gmail.com>
2014-06-10 01:01:24 +0300

    adding test file

---
+++ test/form.php
@@ -0,0 +1,37 @@
+<?php
+
+require '../libs/Form.php';
+require '../libs/Form/Val.php';
+
+try{
+if(isset($_REQUEST['run'])){
+    $form = new Form();
+
+    $form   ->post('name')
+            ->val('minlength', 2)
+            ->post('age')
+            ->val('minlength', 2)
+            ->val('digit')
+            ->post('gender');
+    $form->submit();
+
+    echo 'The form passed!';
+    $data = $form->fetch();
+    echo '<pre>';
+    print_r($data);
+    echo '</pre>';
+}
+} catch (Exception $e ){
+    echo $e->getMessage();
+}
+?>
+
+<form method="post" action="?run">
+    Name: <input type="text" name="name">
+    Age: <input type="text" name="age">
+    Gender: <select name="gender">
+        <option value="m">Male</option>
+        <option value="f">Female</option>
+    </select>
+    <input type="submit" />
+</form>
